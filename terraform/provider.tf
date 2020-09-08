e9d1edc8628a92d721d403196185c2efa6b3f164
Jamie Rajewski <jamie.rajewski1@gmail.com>
2020-09-08 15:52:06 -0600

    Update to latest provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 # Configure the OpenStack Provider
 provider "openstack" {
-  version     = "~> 1.2" # might want to update this later to a more modern version
+  version     = "~> 1.31"
   user_name   = var.username
   tenant_name = "IceCube"
   tenant_id   = "0f4d05d808fb40fe9df8ddd33c576cb2"
